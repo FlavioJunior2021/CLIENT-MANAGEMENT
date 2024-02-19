ed6df014e33191b72fe1fe0cd254a5d7c376f0c2
FlavioJunior2021 <flavinhoj78@gmail.com>
2024-02-19 12:14:27 -0300

    Client routes

--- src/config/routes-config.ts
+++ src/config/routes-config.ts
@@ -1,8 +1,9 @@
+import { clientRoutes } from "../routes/client-route";
 import { orderRoutes } from "../routes/order-route";
 import { app } from "./fastify";
 
 export function registerRoutes() {
-	const routes = [orderRoutes];
+	const routes = [orderRoutes, clientRoutes];
 	routes.forEach((route) => {
 		app.register(route);
 	});
