4c8f851b54af52387a796a5b0bfc1a8b47b4f178
FlavioJunior2021 <flavinhoj78@gmail.com>
2024-02-19 12:14:50 -0300

    Update order-services

--- src/services/order-services.ts
+++ src/services/order-services.ts
@@ -2,11 +2,21 @@ import { prisma } from "../config/prisma";
 import { Order } from "../types/Order";
 
 export async function createOrder(orderData: Order): Promise<Order> {
+	const { description, clientId } = orderData;
+
+	if(!description) throw new Error("From a description to the request to continue");
+
+	if(!clientId) throw new Error("Indicate the clientid of the order");
+
   return await prisma.order.create({ data: orderData });
 }
 
 export async function getOrderById(id: string): Promise<Order> {
-  return await prisma.order.findUniqueOrThrow({ where: { id } });
+	const order = await prisma.order.findUnique({ where: { id }});
+
+	if(!order) throw new Error("Order not found in database");
+
+	return order;
 }
 
 export async function getAllOrders(): Promise<Order[]> {
