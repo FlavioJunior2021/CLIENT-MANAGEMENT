30ba00d19b4d46f9d77b95ec44d7858a57ffec6f
FlavioJunior2021 <flavinhoj78@gmail.com>
2024-02-22 11:09:14 -0300

    Tests: Get Clients by id, Should be get clients by id error

--- src/controller/tests/client-controller.spec.ts
+++ src/controller/tests/client-controller.spec.ts
@@ -1,14 +1,19 @@
-import { createClient, getAllClient } from "../../services/client-services";
+import {
+	createClient,
+	getAllClient,
+	getClientById,
+} from "../../services/client-services";
 import { FastifyReply, FastifyRequest } from "fastify";
 import { ClientController } from "../client-controller";
 
 jest.mock("../../services/client-services");
 
+//Get All Clients
 describe("Client Controller", () => {
 	beforeEach(() => {
 		jest.clearAllMocks();
 	});
-	test("Get Clients Successful", async () => {
+	it("Get Clients Successful", async () => {
 		const mockClients = [
 			{ id: "1", name: "Client 1", phone: "(84) 23344-2244" },
 		];
@@ -32,8 +37,9 @@ describe("Client Controller", () => {
 	});
 });
 
+//Should be error
 describe("Client Controller", () => {
-	test("Get Clients error", async () => {
+	it("Get Clients error", async () => {
 		const mockError = new Error("Internal Server Error");
 
 		const reply = {
@@ -54,6 +60,7 @@ describe("Client Controller", () => {
 	});
 });
 
+//Create Client 
 describe("Client Controller", () => {
 	beforeEach(() => {
 		jest.clearAllMocks();
@@ -64,7 +71,7 @@ describe("Client Controller", () => {
 				name: "Jhon Do",
 				phone: "(84) 865830-9627",
 			},
-		} as unknown as FastifyRequest;
+		} as FastifyRequest;
 
 		const reply = {
 			code: jest.fn().mockReturnThis(),
@@ -83,25 +90,74 @@ describe("Client Controller", () => {
 	});
 });
 
+//Should be create client error
 describe("Client Controller", () => {
-	it("Client created Error and returning 400", async () => {
+	it("Should handle errors and return 400 status code", async () => {
 		const mockError = new Error("Internal Server Error");
 		const request = {
 			body: {
 				name: "Jhon Do",
 				phone: "(84) 865830-9627",
 			},
-		} as unknown as FastifyRequest;
+		} as FastifyRequest;
 
 		const reply = {
 			code: jest.fn().mockReturnThis(),
 			send: jest.fn(),
 		} as unknown as FastifyReply;
-	
-		(createClient as jest.Mock).mockRejectedValue(mockError)
+
+		(createClient as jest.Mock).mockRejectedValue(mockError);
 		await ClientController.create(request, reply);
 
 		expect(reply.send).toHaveBeenCalledWith(mockError);
 		expect(reply.code).toHaveBeenCalledWith(400);
 	});
 });
+
+//Get Clients by id
+describe("Client Controller", () => {
+	beforeEach(() => {
+		jest.clearAllMocks();
+	});
+	it("Get clients by id", async () => {
+		const request = {
+			params: {
+				id: "123-456-789-101123",
+			},
+		} as FastifyRequest;
+
+		const reply = {
+			code: jest.fn().mockReturnThis(),
+			send: jest.fn(),
+		} as unknown as FastifyReply;
+
+		await ClientController.getById(request, reply);
+		expect(getClientById).toHaveBeenCalledTimes(1);
+		expect(getClientById).toHaveBeenCalledWith("123-456-789-101123");
+		expect(reply.code).toHaveBeenCalledWith(201);
+		expect(reply.send).toHaveBeenCalledTimes(1);
+	});
+});
+
+//Should be get clients by id error 
+describe("Client Controller", () => {
+	it("Should handle errors and return 404 status code", async () => {
+		const mockError = new Error("Internal Server Error");
+		const request = {
+			params: {
+				id: "123-456-789-101112",
+			},
+		} as FastifyRequest;
+
+		const reply = {
+			code: jest.fn().mockReturnThis(),
+			send: jest.fn(),
+		} as unknown as FastifyReply;
+
+		(getClientById as jest.Mock).mockRejectedValue(mockError);
+		await ClientController.getById(request, reply);
+
+		expect(reply.send).toHaveBeenCalledWith(mockError);
+		expect(reply.code).toHaveBeenCalledWith(404);
+	})
+})
